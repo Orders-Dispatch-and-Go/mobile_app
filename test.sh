996d3f5aca11dd8be9fe626e4f4c68c0486a9cb1
Bitochkin Egor <bigcubecat@yandex.ru>
2025-11-30 15:03:23 +0700

    пытаюсь добавить toolchain

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 #!/bin/zsh
-clear && cmake -S . -B build/test -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=ON && cmake --build build/test --parallel 4 && ./build/test/appuntitled
+
+clear
+cmake -S . -B build \
+    -DBUILD_TESTING=OFF \
+    -DEXPORT_COMPILE_COMMANDS=ON \
+    -G "Ninja"
+cmake --build build --parallel 6
+./build/appuntitled
